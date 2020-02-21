8f2a476f66e14e77d8400b829cd8ea4718a17578
mona <kjwook7522@gmail.com>
2020-02-22 00:48:16 +0900

    make individual chat contents

--- js/chats.js
+++ js/chats.js
@@ -1,13 +1,28 @@
 const titleIcon = document.querySelectorAll(".title-menu a");
+const plusIcon = document.querySelector(".submit-plus a");
+const submitIcon = document.querySelector(".submit-icon a");
+const chats = document.querySelectorAll(".chats");
+
+console.log(plusIcon);
 
 function warning_massage() {
   alert("Sorry. This fuction is not yet ready.")
+}
+
+function enterChat(order) {
+  location.href = `${order.currentTarget.id}.html`;
 }
 
 function addEvent() {
   titleIcon.forEach(function(node) {
     node.addEventListener("click", warning_massage);
   });
+  chats.forEach(function(node) {
+    node.addEventListener("click", enterChat);
+  });
+  if(plusIcon) {
+    plusIcon.addEventListener("click", warning_massage);
+  }
 }
 
 function init() {
