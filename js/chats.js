6ca3659f12f0d42d97100346e9e6dc43555329ea
mona <kjwook7522@gmail.com>
2020-02-23 12:02:25 +0900

    add save and delete function to chats

--- js/chats.js
+++ js/chats.js
@@ -1,5 +1,7 @@
 const titleIcon = document.querySelectorAll(".title-menu a");
 const plusIcon = document.querySelector(".submit-plus a");
+const menuPopup = document.querySelector(".title-menu-popup");
+const menuLayer = document.querySelector(".title-menu-layer");
 const submitForm = document.querySelector(".submit-input");
 const submitInput = document.querySelector(".submit-input input");
 const submitIcon = document.querySelector(".submit-icon a");
@@ -31,22 +33,71 @@ function createDate() {
   return time;
 }
 
+function showMenuPopup() {
+  menuPopup.style.display = "flex";
+  menuLayer.style.display = "block";
+  menuPopup.addEventListener("click", deleteTalk);
+  menuLayer.addEventListener("click", hideMenuPopup);
+}
+
+function hideMenuPopup() {
+  menuPopup.style.display = "none";
+  menuLayer.style.display = "none";
+}
+
 function submitMessage(event) {
   event.preventDefault();
-  const time = createDate();
+  const text = submitInput.value;
 
+  if(text) {
+    paintTalk(text);
+    saveTalk(text);
+  }
+  submitInput.value = "";
+}
+
+function saveTalk(text) {
+  const talkObj = new Object();
+  const date = new Date();
+  
+  talkObj.month = date.getMonth() + 1;
+  talkObj.day = date.getDay();
+  talkObj.hours = date.getHours();
+  talkObj.minutes = date.getMinutes();
+  talkObj.text = text;
+  talkObj.who = "leejaeyong";
+  talkObj.youandme = "me";
+
+  const talks = [];
+  const data = JSON.parse(localStorage.getItem("talks"));
+  if(data) {
+    data.forEach(function(talk) {
+      talks.push(talk);
+    });
+  }
+  talks.push(talkObj);
+  localStorage.setItem("talks", JSON.stringify(talks));
+}
+
+function deleteTalk() {
+  localStorage.removeItem("talks");
+  location.reload();
+}
+
+function paintTalk(text) {
+  const time = createDate();
   const chat = document.querySelector(".chats-contents");
 
-  const text = document.createElement("h1");
-  const textNode = document.createTextNode(submitInput.value);
-  text.appendChild(textNode);
+  const content = document.createElement("h1");
+  const contentNode = document.createTextNode(text);
+  content.appendChild(contentNode);
 
   const date = document.createElement("h2");
   const dateNode = document.createTextNode(time);
   date.appendChild(dateNode);
 
   const message = document.createElement("div");
-  message.appendChild(text);
+  message.appendChild(content);
   message.appendChild(date);
   message.classList.add("chats-text");
 
@@ -56,14 +107,26 @@ function submitMessage(event) {
   chat.appendChild(talk);
 
   window.scrollTo(0,document.body.scrollHeight);
+}
 
-  submitInput.value = "";
+function loadTalk() {
+  const talks = [];
+  const data = JSON.parse(localStorage.getItem("talks"));
+  if(data) {
+    data.forEach(function(talk) {
+      talks.push(talk);
+    });
+  }
+  talks.forEach(function(talk) {
+    paintTalk(talk.text);
+  });
 }
 
 function addEvent() {
-  titleIcon.forEach(function(node) {
-    node.addEventListener("click", warning_massage);
-  });
+  if(titleIcon) {
+    titleIcon[0].addEventListener("click", warning_massage);
+    titleIcon[1].addEventListener("click", showMenuPopup);
+  }
   chats.forEach(function(node) {
     node.addEventListener("click", enterChat);
   });
@@ -74,13 +137,13 @@ function addEvent() {
     submitForm.addEventListener("submit", submitMessage);
   }
   if(submitIcon) {
-    submitIcon.addEventListener("click", warning_massage);
+    submitIcon.addEventListener("click", submitMessage);
   }
 }
 
 function init() {
   addEvent();
-  createDate();
+  loadTalk();
 }
 
 init();
